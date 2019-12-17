580b93072e0d3b89fecbc8dbca9b54b7a6859ea5
Алексей Жуков <aleksei.zhukov8102@gmail.com>
2019-12-17 19:44:51 +0300

    удалил компонент Cell, перенос функционала

--- sea_battle/src/components/Field/Field.js
+++ sea_battle/src/components/Field/Field.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Cell } from './../Routing';
 import './Field.css';
+import {config} from "../../Config";
 
 /**
  * TODO интегрировать Cell компоннет
@@ -12,18 +13,54 @@ import './Field.css';
  * @constructor
  */
 const Field = ({ field, mode, playFor, handleClick }) => {
+
+    const getClassAndId = (value) => {
+        let className = 'cell',
+            idName = 'empty';
+
+        if (value === (-1) * config.safeValue) {
+            idName = 'missed';
+        }
+        else if (value < 0) {
+            idName = 'killed';
+        }
+
+        if ((mode === 'play' && playFor === 'AI') ||
+            (mode === 'prepare' && playFor === 'player')) {
+
+            if (!value || Math.abs(value) === config.safeValue) {
+                className += ' empty';
+            }
+        } else {
+            if (idName !== 'killed') {
+                className += ' empty';
+            }
+        }
+        return {
+            idName: idName,
+            className: className
+        };
+    };
+
     return(
         <div id="field">
             {
                 field.map((row, rowIndex) =>
                     <div id="row" key={rowIndex}>
                         {
-                            row.map((cell, index) =>
-                                <Cell key={index}
-                                      value={cell}
-                                      handleClick={() => handleClick(rowIndex, index, playFor)}
-                                      mode={mode}
-                                      playFor={playFor}/>)
+                            row.map((cell, index) => {
+                                const { idName, className } = getClassAndId(cell);
+                                return (
+                                    <div className={className}
+                                         onClick={() => handleClick(rowIndex, index, playFor)}
+                                         key={index}>
+
+                                        <div id={idName}>
+                                            {idName === 'killed' && <img src="./img/fire.png" alt="killed"/>}
+                                        </div>
+                                    </div>
+                                )
+                            })
                         }
                     </div>
                 )
