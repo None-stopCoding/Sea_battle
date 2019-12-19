a0c4f3f73c839775bdc0e2c08c1786161ab74fa8
PAndreev8876 <58572171+PAndreev8876@users.noreply.github.com>
2019-12-19 16:33:53 +0300

    Сделал страницу регистрации

---
+++ sea_battle/modal/script.js
@@ -0,0 +1,7 @@
+/* Модальное окно */
+var modal = document.getElementById('id01');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
