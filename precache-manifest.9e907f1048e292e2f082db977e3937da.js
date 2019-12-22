381748437214fb13720b706a24879d7be5ef7539
Алексей Жуков <aleksei.zhukov8102@gmail.com>
2019-12-22 15:46:39 +0300

    сборка production

---
+++ precache-manifest.9e907f1048e292e2f082db977e3937da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d66189a966735caafd9a1ace221e9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5452df555d27998b3d78",
+    "url": "/static/css/main.7eff5f25.chunk.css"
+  },
+  {
+    "revision": "1864600e77cb3ccb4a0f",
+    "url": "/static/js/2.e19e3501.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e19e3501.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5452df555d27998b3d78",
+    "url": "/static/js/main.b574e631.chunk.js"
+  },
+  {
+    "revision": "fd0983df5a5967c3f421",
+    "url": "/static/js/runtime-main.9aefd2dd.js"
+  }
+]);
